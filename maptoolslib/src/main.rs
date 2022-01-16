a64addf38ef5bbbe428eb27335c63b2557bf060c
Stephen Catsamas <seca8193@gmail.com>
2022-01-17 00:22:04 +1100

    got rust segmenter working quite slow file writing time colours off on the rust segmenter

--- maptoolslib/src/main.rs
+++ maptoolslib/src/main.rs
@@ -7,45 +7,20 @@ use std::time::{Instant};
 
 fn main()  {
     let mut tik = Instant::now();
-    seg::load_map();
+    let segemnts = seg::load_map("map.osm");
+    let lines = seg::format(segemnts);
+    seg::tofile("segout.seg", lines);
     println!("t0: {}", tik.elapsed().as_millis());
+    // render_test();
 
 }
 
-
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-// This is a really bad adding function, its purpose is to fail in this
-// example.
-#[allow(dead_code)]
-fn bad_add(a: i32, b: i32) -> i32 {
-    a - b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-
-    #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(bad_add(1, 2), 3);
-    }
-}
 
 #[allow(dead_code)]
 fn render_test() {
     let mut tik = Instant::now();
 
-    let lines = draw::linesfromhex("seg.seg", 0.001);
+    let lines = draw::linesfromhex("segout.seg", 0.001);
     let view = View { bounds: [-37.608,-37.94,145.173,144.72], res: 4096.0 };
 
     println!("t0: {}", tik.elapsed().as_millis());
--- maptoolslib/src/seg.rs
+++ maptoolslib/src/seg.rs
@@ -1,11 +1,15 @@
+use std::f32::consts::TAU as PI2;
 use std::collections::HashMap;
-use std::io::BufReader;
+use std::io::{Write,BufReader};
 use std::fs::File;
 use quick_xml as qxml;
 use qxml::{Reader, events::Event};
 
-struct Way {
-    nodes : Vec<u64>,
+use crate::renderer::{Line};
+
+#[derive(Debug)]
+pub struct Way {
+    pub nodes : Vec<u64>,
 }
 
 impl Way {
@@ -17,12 +21,82 @@ impl Way {
     }
 }
 
-pub fn load_map(){
+pub fn tofile(fp :  &str, lines : Vec<Line>){
+
+    let mut f = File::create(fp).unwrap();
+
+    for line in lines{
+
+        for flt in line.to{
+            let bts = flt.to_ne_bytes();
+            f.write(&bts).unwrap();
+        }
+        for flt in line.from{
+            let bts = flt.to_ne_bytes();
+            f.write(&bts).unwrap();
+        }
+        for flt in line.colour{
+            let bts = flt.to_ne_bytes();
+            f.write(&bts).unwrap();
+        }
+    }
+
+}
+
+pub fn format(segs : (Vec::<Way>,HashMap<u64, [f32;2]>)) -> Vec<Line>{
+    let (ways, nodes) = segs;
+
+    let mut lines = Vec::<Line>::new();
+
+    for way in ways{
+        let niter = way.nodes.windows(2);//get node ids 2 at a time
+        for nodeids in niter{
+            let toid = nodeids.get(0).unwrap();
+            let fmid = nodeids.get(1).unwrap();
+            let to : [f32;2] = nodes[toid];
+            let from : [f32;2] = nodes[fmid];
+
+            let colour = colourfunc(to,from);
+            let width = widthfunc(to,from);
+
+            let line : Line = Line {to,from,colour,width};
+
+            lines.push(line);
+
+        }
+
+
+    }
+
+    return lines;
+
+}
+
+fn colourfunc(to : [f32;2], from : [f32;2]) -> [f32;3]{
+    let dlat = to[0] - from[0];
+    let dlon = to[1] - from[1];
+    
+    let x = (to[0].to_radians()).cos() * dlon;
+    let y = dlat;
+    
+    let ang = y.atan2(x);
+    
+    let h = (4.0*ang/PI2) % 1.0;
+    let s = 0.6;
+    let v = 1.0;
+    return [h,s,v]
+}
+
+fn widthfunc(to : [f32;2], from : [f32;2]) -> f32{
+    0.1
+}
+
+pub fn load_map(fp : &str) -> (Vec::<Way>,HashMap<u64, [f32;2]>){
 
     let mut nodehash = HashMap::<u64, [f32;2]>::new();
     let mut wayvec = Vec::<Way>::new();
 
-    let mut reader = Reader::from_file("map.osm").unwrap();
+    let mut reader = Reader::from_file(fp).unwrap();
     reader.trim_text(true);
 
     let mut buf = Vec::new();
@@ -39,9 +113,9 @@ pub fn load_map(){
                         nodehash.insert(node.0, node.1);
                         ()},
                     b"way" => {
-                        let way = xml_way(&mut reader, &mut wbuf, e);
-                        wayvec.push(way);
-                        ()},
+                            let way = xml_way(&mut reader, &mut wbuf, e);
+                            wayvec.push(way);
+                            ()},
                     _ => {()},
                 }
             },
@@ -61,8 +135,7 @@ pub fn load_map(){
             _ => (), 
         }
     }
-   println!("wayvec {}", wayvec.len()); 
-   println!("nodehash {}", nodehash.keys().len()); 
+    (wayvec,nodehash)
 }
 
 fn xml_node(e : &qxml::events::BytesStart) -> (u64, [f32;2]){
@@ -79,10 +152,11 @@ fn xml_node(e : &qxml::events::BytesStart) -> (u64, [f32;2]){
         }
 
     }
+
     return node;
 }
 
-//should probably use Text(BytesText<'a>) event
+
 fn xml_nd(e : &qxml::events::BytesStart) -> u64{
     let aiter = e.attributes(); //itterator over attributes
 
@@ -90,21 +164,21 @@ fn xml_nd(e : &qxml::events::BytesStart) -> u64{
     for attr in aiter{
         let attr = attr.unwrap();
         match attr.key {
-            b"nd" => {nd = lexical::parse::<u64, _>(attr.value).unwrap();},
+            b"ref" => {nd = lexical::parse::<u64, _>(attr.value).unwrap();},
             _ => {},
         }
     }
     return nd;
 }
 
-fn xml_way(r : &mut Reader<BufReader<File>>, mut buf : &mut Vec<u8>, e : &qxml::events::BytesStart) -> Way{
+fn xml_way(r : &mut Reader<BufReader<File>>, mut buf : &mut Vec<u8>, es : &qxml::events::BytesStart) -> Way{
     let mut way = Way::new();
     
-    let nm = e.name();
+    let nm = es.name();
     loop {
         match r.read_event(&mut buf) {
 
-            Ok(Event::Start(ref e)) => {
+            Ok(Event::Empty(ref e)) => {
 
                 match e.name() {
                     b"nd" => {
@@ -127,5 +201,5 @@ fn xml_way(r : &mut Reader<BufReader<File>>, mut buf : &mut Vec<u8>, e : &qxml::
             _ => (), 
         }
     } 
-    return Way::new();
+    return way;
 }
